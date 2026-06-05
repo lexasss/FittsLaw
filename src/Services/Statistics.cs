745eb9a3f787028770ef403f2d94ab80360f5203
lexasss <oleg.spakov@tuni.fi>
2026-06-05 14:07:57 +0300

    a plot in Statistics

--- src/Services/Statistics.cs
+++ src/Services/Statistics.cs
@@ -2,24 +2,40 @@
 
 internal class Statistics
 {
+    public static string[] Fields => [
+        "Block",
+        "Trials",
+        "Amplitude, px",
+        "Width, px",
+        "Offset, px",
+        "ID, bits",
+        "Eff. Amplitude, px",
+        "Eff. Width, px",
+        "Eff. ID, bits",
+        "MT, ms",
+        "Errors",
+        "Errors, %",
+        "Throughput, b/s"
+    ];
+
     public IReadOnlyDictionary<string, string[]> Compute(Models.Block[] experimentBlocks)
     {
         int n = experimentBlocks.Length;
 
         Dictionary<string, string[]> result = [];
-        result.Add("Block", new string[n]);
-        result.Add("Trials", new string[n]);
-        result.Add("Amplitude, px", new string[n]);
-        result.Add("Width, px", new string[n]);
-        result.Add("Offset, px", new string[n]);
-        result.Add("ID, bits", new string[n]);
-        result.Add("Eff. Amplitude, px", new string[n]);
-        result.Add("Eff. Width, px", new string[n]);
-        result.Add("Eff. ID, bits", new string[n]);
-        result.Add("MT, ms", new string[n]);
-        result.Add("Errors", new string[n]);
-        result.Add("Errors, %", new string[n]);
-        result.Add("Throughput, b/s", new string[n]);
+        result.Add(Fields[0], new string[n]);
+        result.Add(Fields[1], new string[n]);
+        result.Add(Fields[2], new string[n]);
+        result.Add(Fields[3], new string[n]);
+        result.Add(Fields[4], new string[n]);
+        result.Add(Fields[5], new string[n]);
+        result.Add(Fields[6], new string[n]);
+        result.Add(Fields[7], new string[n]);
+        result.Add(Fields[8], new string[n]);
+        result.Add(Fields[9], new string[n]);
+        result.Add(Fields[10], new string[n]);
+        result.Add(Fields[11], new string[n]);
+        result.Add(Fields[12], new string[n]);
 
         experimentBlocks.Sort((b1, b2) => b1.Index.CompareTo(b2.Index));
 
@@ -62,10 +78,10 @@ internal class Statistics
                 startY = activationY;
             }
 
-            result["Block"][i] = (i + 1).ToString();
-            result["Trials"][i] = block.Targets.Count().ToString();
-            result["Amplitude, px"][i] = block.Amplitude.ToString();
-            result["Width, px"][i] = block.Width.ToString();
+            result[Fields[0]][i] = (i + 1).ToString();
+            result[Fields[1]][i] = block.Targets.Count().ToString();
+            result[Fields[2]][i] = block.Amplitude.ToString();
+            result[Fields[3]][i] = block.Width.ToString();
 
             n = block.Targets.Count() - 1;
 
@@ -83,27 +99,27 @@ internal class Statistics
                 var effectiveWidth = 4.133 * sd;
                 var effectiveId = effectiveWidth > 0 ? Math.Log2(effectiveAmplitude / effectiveWidth + 1) : 0;
 
-                result["Offset, px"][i] = meanOffset.ToString("F1");
-                result["ID, bits"][i] = id.ToString("F2");
-                result["Eff. Amplitude, px"][i] = effectiveAmplitude.ToString("F1");
-                result["Eff. Width, px"][i] = effectiveWidth.ToString("F1");
-                result["Eff. ID, bits"][i] = effectiveId.ToString("F2");
-                result["MT, ms"][i] = meanDuration.ToString();
-                result["Errors"][i] = errorCount.ToString();
-                result["Errors, %"][i] = (100.0 * errors).ToString("F1");
-                result["Throughput, b/s"][i] = throughput.ToString("F2");
+                result[Fields[4]][i] = meanOffset.ToString("F1");
+                result[Fields[5]][i] = id.ToString("F2");
+                result[Fields[6]][i] = effectiveAmplitude.ToString("F1");
+                result[Fields[7]][i] = effectiveWidth.ToString("F1");
+                result[Fields[8]][i] = effectiveId.ToString("F2");
+                result[Fields[9]][i] = meanDuration.ToString();
+                result[Fields[10]][i] = errorCount.ToString();
+                result[Fields[11]][i] = (100.0 * errors).ToString("F1");
+                result[Fields[12]][i] = throughput.ToString("F2");
             }
             else // otherwise the computed values are set to 0 indicating the block is invalid for analysis
             {
-                result["Offset, px"][i] = Zero;
-                result["ID, bits"][i] = Zero;
-                result["Eff. Amplitude, px"][i] = Zero;
-                result["Eff. Width, px"][i] = Zero;
-                result["Eff. ID, bits"][i] = Zero;
-                result["MT, ms"][i] = Zero;
-                result["Errors"][i] = Zero;
-                result["Errors, %"][i] = Zero;
-                result["Throughput, b/s"][i] = Zero;
+                result[Fields[4]][i] = Zero;
+                result[Fields[5]][i] = Zero;
+                result[Fields[6]][i] = Zero;
+                result[Fields[7]][i] = Zero;
+                result[Fields[8]][i] = Zero;
+                result[Fields[9]][i] = Zero;
+                result[Fields[10]][i] = Zero;
+                result[Fields[11]][i] = Zero;
+                result[Fields[12]][i] = Zero;
             }
 
             i++;
--- src/ViewModels/Statistics.cs
+++ src/ViewModels/Statistics.cs
@@ -1,7 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using ScottPlot;
+using System.Drawing;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
 
 namespace FittsLaw.ViewModels;
 
@@ -13,12 +17,58 @@ internal partial class Statistics : ObservableObject
     [ObservableProperty]
     public partial IReadOnlyDictionary<string, string[]> Items { get; set; } = new Dictionary<string, string[]>();
 
+    public WpfPlot PlotControl { get; } = new WpfPlot();
+
     public event EventHandler? HideCopyToClipboardConfirmation;
+
+    public Statistics()
+    {
+        PlotControl.Plot.Legend(location: Alignment.UpperLeft);
+        PlotControl.Plot.BottomAxis.Label(Services.Statistics.Fields[5]);
+        PlotControl.Plot.LeftAxis.Label(Services.Statistics.Fields[12]);
+        PlotControl.Plot.RightAxis.Label(Services.Statistics.Fields[9]);
+        PlotControl.Plot.RightAxis.Ticks(true);
+    }
+
+    #region Property Setters
+
+    partial void OnItemsChanged(IReadOnlyDictionary<string, string[]> value)
+    {
+        PlotControl.Plot.Clear();
+
+        if (!value.ContainsKey(Services.Statistics.Fields[5]) || 
+            !value.ContainsKey(Services.Statistics.Fields[12]) || 
+            !value.ContainsKey(Services.Statistics.Fields[9]))
+            return;
+
+        var dataX = value.FirstOrDefault(kv => kv.Key == Services.Statistics.Fields[5])
+            .Value
+            .Select(double.Parse)
+            .ToArray();
+        var dataY = value.FirstOrDefault(kv => kv.Key == Services.Statistics.Fields[12])
+            .Value
+            .Select(double.Parse)
+            .ToArray();
+        PlotControl.Plot
+            .AddScatter(dataX, dataY, Color.Blue, lineStyle: LineStyle.None, label: "TP", markerShape: MarkerShape.cross, markerSize: 10);
+
+        dataY = value.FirstOrDefault(kv => kv.Key == Services.Statistics.Fields[9])
+            .Value
+            .Select(double.Parse)
+            .ToArray();
+        PlotControl.Plot
+            .AddScatter(dataX, dataY, Color.Red, lineStyle: LineStyle.None, label: "MT")
+            .YAxisIndex = 1;
+
+        PlotControl.Refresh();
+    }
+
+    #endregion
 
     #region Commands
 
     [RelayCommand]
-    private void CopyToClipboard()
+    private void CopyTableToClipboard()
     {
         List<string> lines = [];
 
@@ -35,13 +85,62 @@ internal partial class Statistics : ObservableObject
 
         Clipboard.SetText(string.Join('\n', lines));
 
+        ShowConfirmation();
+    }
+
+
+    [RelayCommand]
+    private void CopyPlotToClipboard()
+    {
+        if (Keyboard.Modifiers.HasFlag(ModifierKeys.Control))
+        {
+            Clipboard.SetText(PlotControl.Plot.GetImageHtml());
+        }
+        else
+        {
+            Clipboard.SetImage(ToBitmapSource(PlotControl.Plot.GetBitmap()));
+        }
+
+        ShowConfirmation();
+    }
+
+    #endregion
+
+    #region Internal
+
+    const int ConfirmationVisibilityDuration = 2000;
+
+    private void ShowConfirmation()
+    {
         CopyToClipboardConfirmationVisibility = Visibility.Visible;
         Task.Run(async () =>
         {
-            await Task.Delay(2000);
+            await Task.Delay(ConfirmationVisibilityDuration);
             HideCopyToClipboardConfirmation?.Invoke(this, EventArgs.Empty);
         });
     }
+
+    private static BitmapSource ToBitmapSource(Bitmap bitmap)
+    {
+        IntPtr hBitmap = bitmap.GetHbitmap();
+
+        try
+        {
+            return Imaging.CreateBitmapSourceFromHBitmap(
+                hBitmap,
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+        }
+        finally
+        {
+            DeleteObject(hBitmap);
+        }
+    }
+
+    [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+    [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
+    static extern bool DeleteObject(IntPtr hObject);
 
     #endregion
 }
