795d6575ade0e59561c344eb641c2c934ed935b9
lexasss <oleg.spakov@tuni.fi>
2026-06-05 12:10:56 +0300

    active target is always on top

---
+++ src/Converters/TargetIsActiveToZIndex.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace FittsLaw.Converters;
+
+[ValueConversion(typeof(bool), typeof(int))]
+internal class TargetIsActiveToZIndex : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => (bool)value ? 1 : 0;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => false;
+}
