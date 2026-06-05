1fd131b567965c84ad285b5b845312a7d5299362
lexasss <oleg.spakov@tuni.fi>
2026-06-05 14:37:31 +0300

    UI improvement in Statistics

---
+++ src/Converters/StatKeyToMargin.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FittsLaw.Converters;
+
+[ValueConversion(typeof(string), typeof(Thickness))]
+public class StatKeyToMargin : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (string)value == Services.Statistics.Fields[4]
+            ? new Thickness(8, 8, 8, 2)
+            : new Thickness(8, 2, 8, 2);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
