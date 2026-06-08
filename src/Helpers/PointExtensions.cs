1371ced9b926d5da19d12402a571afb49f98bdae
lexasss <oleg.spakov@tuni.fi>
2026-06-08 17:44:35 +0300

    bugfix

--- src/Helpers/PointExtensions.cs
+++ src/Helpers/PointExtensions.cs
@@ -7,7 +7,7 @@ internal static class PointExtensions
 #if VS_VERSION_18_0_OR_GREATER
     extension(Point pt)
     {
-        public static double Amplitude() => Math.Sqrt(pt.X * pt.X + pt.Y * pt.Y);
+        public double Amplitude() => Math.Sqrt(pt.X * pt.X + pt.Y * pt.Y);
     }
 #else
     public static double Amplitude(this Point point) => Math.Sqrt(point.X * point.X + point.Y * point.Y);
